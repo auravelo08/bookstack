e37e1cb8dbcf7b571fa381db0c465ab07762cf51
aurelia <auravelo08@example.com>
2026-05-22 15:53:35 +0200

    edit gitlab et locust file

--- locustfile.py
+++ locustfile.py
@@ -1,13 +1,18 @@
 from locust import HttpUser, task, between
+from bs4 import BeautifulSoup
 
 class BookStackUser(HttpUser):
     wait_time = between(1, 3)  # attente entre chaque action
 
     def on_start(self):
+        response = self.client.get("/login")
+        soup = BeautifulSoup(response.text, "html.parser")
+        token = soup.find("input", {"name": "_token"})["value"]
         # appelé une fois au démarrage de chaque utilisateur
         self.client.post("/login", {
             "email": "admin@admin.com",
-            "password": "password"
+            "password": "password",
+            "_token": token
         })
 
     @task
